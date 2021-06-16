59fd82a2663101e19d985373d343788874184134
William Wallace <me@wiox.me>
2021-06-16 19:40:48 +0100

    untested sql thingy

--- dmsrc/sql.dm
+++ dmsrc/sql.dm
@@ -1,6 +1,10 @@
+#define RUSTG_SQL_ASYNC_PENDING "SQL ASYNC PENDING"
+#define RUSTG_SQL_ASYNC_UNKNOWN_QUERY "SQL ASYNC UNKNOWN QUERY"
+#define RUSTG_SQL_ASYNC_UNKNOWN_CONNECTION "SQL ASYNC UNKNOWN CONNECTION"
+
 #define rustg_sql_connect_pool(options) call(RUST_G, "sql_connect_pool")(options)
 #define rustg_sql_query_async(handle, query, params) call(RUST_G, "sql_query_async")(handle, query, params)
 #define rustg_sql_query_blocking(handle, query, params) call(RUST_G, "sql_query_blocking")(handle, query, params)
 #define rustg_sql_connected(handle) call(RUST_G, "sql_connected")(handle)
 #define rustg_sql_disconnect_pool(handle) call(RUST_G, "sql_disconnect_pool")(handle)
-#define rustg_sql_check_query(job_id) call(RUST_G, "sql_check_query")("[job_id]")
+#define rustg_sql_check_query(handle, job_id) call(RUST_G, "sql_check_query")(handle, "[job_id]")
--- src/sql.rs
+++ src/sql.rs
@@ -1,4 +1,3 @@
-use crate::jobs;
 use dashmap::DashMap;
 use mysql::{
     consts::{ColumnFlags, ColumnType::*},
@@ -8,7 +7,7 @@ use mysql::{
 use once_cell::sync::Lazy;
 use serde::Deserialize;
 use serde_json::{json, map::Map, Number};
-use std::sync::atomic::AtomicUsize;
+use std::{mem::ManuallyDrop, sync::{Arc, atomic::AtomicUsize}, thread::JoinHandle};
 use std::{error::Error, time::Duration};
 
 // ----------------------------------------------------------------------------
@@ -16,8 +15,13 @@ use std::{error::Error, time::Duration};
 
 const DEFAULT_PORT: u16 = 3306;
 // The `mysql` crate defaults to 10 and 100 for these, but that is too large.
-const DEFAULT_MIN_THREADS: usize = 1;
-const DEFAULT_MAX_THREADS: usize = 10;
+const DEFAULT_MIN_CONNECTIONS: usize = 1;
+const DEFAULT_MAX_CONNECTIONS: usize = 10;
+
+
+const SQL_ASYNC_PENDING: &str = "SQL ASYNC PENDING";
+const SQL_ASYNC_UNKNOWN_QUERY: &str = "SQL ASYNC UNKNOWN QUERY";
+const SQL_ASYNC_UNKNOWN_CONNECTION: &str = "SQL ASYNC UNKNOWN CONNECTION";
 
 #[derive(Deserialize)]
 struct ConnectOptions {
@@ -32,119 +36,212 @@ struct ConnectOptions {
     max_threads: Option<usize>,
 }
 
+type QueueId = String;
+type QueryId = String;
+
+static NEXT_CONNECTION_ID: AtomicUsize = AtomicUsize::new(0);
+static NEXT_QUERY_ID: AtomicUsize = AtomicUsize::new(0);
+static CONNECTIONS: Lazy<DashMap<QueueId, Connection>> = Lazy::new(DashMap::new);
+
+struct QueuedQuery {
+    id: QueryId,
+    query: String,
+    params: String,
+}
+
+struct Connection {
+    sender: ManuallyDrop<crossbeam_channel::Sender<QueuedQuery>>,
+    results: Arc<DashMap<QueryId, Option<String>>>,
+    workers: Vec<JoinHandle<()>>,
+    pool: mysql::Pool,
+}
+
+impl Connection {
+    fn create(options: ConnectOptions) -> Result<serde_json::Value, Box<dyn Error>> {
+        let builder = OptsBuilder::new()
+            .ip_or_hostname(options.host)
+            .tcp_port(options.port.unwrap_or(DEFAULT_PORT))
+            // Work around addresses like `localhost:3307` defaulting to socket as
+            // if the port were the default too.
+            .prefer_socket(options.port.map_or(true, |p| p == DEFAULT_PORT))
+            .user(options.user)
+            .pass(options.pass)
+            .db_name(options.db_name)
+            .read_timeout(options.read_timeout.map(Duration::from_secs_f32))
+            .write_timeout(options.write_timeout.map(Duration::from_secs_f32));
+
+        let min_connections = options.min_threads.unwrap_or(DEFAULT_MIN_CONNECTIONS);
+        let max_connections = options.max_threads.unwrap_or(DEFAULT_MAX_CONNECTIONS);
+
+        let pool = Pool::new_manual(
+            min_connections,
+            max_connections,
+            builder,
+        )?;
+
+        let (sender, receiver) = crossbeam_channel::unbounded();
+        let results = Arc::new(DashMap::new());
+
+        let workers = (0..max_connections).map(|_| {
+            let receiver = receiver.clone();
+            let results = results.clone();
+            let pool = pool.clone();
+            std::thread::spawn(move || {
+                Self::worker(receiver, results, pool)
+            })
+        }).collect();
+
+        let handle = NEXT_CONNECTION_ID.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+        let handle = handle.to_string();
+        CONNECTIONS.insert(handle.clone(), Self {
+            sender: ManuallyDrop::new(sender),
+            results,
+            workers,
+            pool,
+        });
+
+        Ok(json!({
+            "status": "ok",
+            "handle": handle,
+        }))
+    }
+
+    fn query_blocking(&self, query: &str, params: &str) -> Result<serde_json::Value, Box<dyn Error>> {
+        let mut conn = self.pool.get_conn()?;
+        let inner = conn.as_mut();
+        do_query(inner, &query, &params)
+    }
+
+    fn query_async(&self, query: &str, params: &str) -> Result<String, Box<dyn Error>> {
+        let id = NEXT_QUERY_ID.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
+        let id = id.to_string();
+
+        self.results.insert(id.clone(), None);
+        self.sender.send(QueuedQuery {
+            id: id.clone(),
+            query: query.to_owned(),
+            params: params.to_owned(),
+        }).unwrap();
+
+        Ok(id)
+    }
+
+    fn check_async(&self, id: &str) -> String {
+        match self.results.get(id) {
+            Some(res) => {
+                match &*res {
+                    Some(_) => {
+                        self.results.remove(id).unwrap().1.unwrap()
+                    }
+                    None => SQL_ASYNC_PENDING.to_owned(),
+                }
+            }
+
+            None => SQL_ASYNC_UNKNOWN_QUERY.to_owned(),
+        }
+    }
+
+    fn worker(receiver: crossbeam_channel::Receiver<QueuedQuery>, results: Arc<DashMap<QueryId, Option<String>>>, pool: mysql::Pool) {
+        while let Ok(query) = receiver.recv() {
+            let mut conn = match pool.get_conn() {
+                Ok(conn) => conn,
+                Err(e) => {
+                    results.insert(query.id, Some(err_to_json(e)));
+                    continue;
+                }
+            };
+            let inner = conn.as_mut();
+
+            let result = match do_query(inner, &query.query, &query.params) {
+                Ok(o) => o.to_string(),
+                Err(e) => err_to_json(e),
+            };
+
+            results.insert(query.id, Some(result));
+        }
+    }
+}
+
+impl Drop for Connection {
+    fn drop(&mut self) {
+        // Wait for ongoing queries to finish
+        unsafe {
+            ManuallyDrop::drop(&mut self.sender);
+        }
+
+        while let Some(worker) = self.workers.pop() {
+            worker.join().unwrap();
+        }
+    }
+}
+
 byond_fn! { sql_connect_pool(options) {
     let options = match serde_json::from_str::<ConnectOptions>(options) {
         Ok(options) => options,
         Err(e) => return Some(err_to_json(e)),
     };
-    Some(match sql_connect(options) {
+    Some(match Connection::create(options) {
         Ok(o) => o.to_string(),
         Err(e) => err_to_json(e)
     })
 } }
 
 byond_fn! { sql_query_blocking(handle, query, params) {
-    Some(match do_query(handle, query, params) {
-        Ok(o) => o.to_string(),
-        Err(e) => err_to_json(e)
-    })
+    match CONNECTIONS.get(handle) {
+        Some(connection) => {
+            let res = match connection.query_blocking(query, params) {
+                Ok(o) => o.to_string(),
+                Err(e) => err_to_json(e)
+            };
+
+            Some(res)
+        }
+
+        None => Some(json!({"status": "offline"}).to_string()),
+    }
 } }
 
 byond_fn! { sql_query_async(handle, query, params) {
-    let handle = handle.to_owned();
-    let query = query.to_owned();
-    let params = params.to_owned();
-    Some(jobs::start(move || {
-        match do_query(&handle, &query, &params) {
-            Ok(o) => o.to_string(),
-            Err(e) => err_to_json(e)
+    match CONNECTIONS.get(handle) {
+        Some(connection) => {
+            let res = match connection.query_async(query, params) {
+                Ok(o) => o.to_string(),
+                Err(e) => err_to_json(e)
+            };
+
+            Some(res)
         }
-    }))
+
+        None => Some(json!({"status": "offline"}).to_string()),
+    }
 } }
 
 // hopefully won't panic if queries are running
 byond_fn! { sql_disconnect_pool(handle) {
-    let handle = match handle.parse::<usize>() {
-        Ok(o) => o,
-        Err(e) => return Some(err_to_json(e)),
-    };
-    Some(
-         match POOL.remove(&handle) {
-            Some(_) => {
-                json!({
-                    "status": "success"
-                }).to_string()
-            },
-            None => json!({
-                "status": "offline"
-            }).to_string()
-        }
-    )
+    match CONNECTIONS.remove(handle) {
+        Some(_) => Some(json!({"status": "success"}).to_string()),
+        None => Some(json!({"status": "offline"}).to_string()),
+    }
 } }
 
 byond_fn! { sql_connected(handle) {
-    let handle = match handle.parse::<usize>() {
-        Ok(o) => o,
-        Err(e) => return Some(err_to_json(e)),
-    };
-    Some(
-        match POOL.get(&handle) {
-            Some(_) => json!({
-                "status": "online"
-            }).to_string(),
-            None => json!({
-                "status": "offline"
-            }).to_string()
+    match CONNECTIONS.remove(handle) {
+        Some(_) => Some(json!({"status": "online"}).to_string()),
+        None => Some(json!({"status": "offline"}).to_string()),
+    }
+} }
+
+byond_fn! { sql_check_query(handle, id) {
+    match CONNECTIONS.get(handle) {
+        Some(connection) => {
+            Some(connection.check_async(id))
         }
-    )
+
+        None => Some(SQL_ASYNC_UNKNOWN_CONNECTION.to_owned())
+    }
 } }
 
-byond_fn! { sql_check_query(id) {
-    Some(jobs::check(id))
-} }
-
-// ----------------------------------------------------------------------------
-// Main connect and query implementation
-
-static POOL: Lazy<DashMap<usize, Pool>> = Lazy::new(DashMap::new);
-static NEXT_ID: AtomicUsize = AtomicUsize::new(0);
-
-fn sql_connect(options: ConnectOptions) -> Result<serde_json::Value, Box<dyn Error>> {
-    let builder = OptsBuilder::new()
-        .ip_or_hostname(options.host)
-        .tcp_port(options.port.unwrap_or(DEFAULT_PORT))
-        // Work around addresses like `localhost:3307` defaulting to socket as
-        // if the port were the default too.
-        .prefer_socket(options.port.map_or(true, |p| p == DEFAULT_PORT))
-        .user(options.user)
-        .pass(options.pass)
-        .db_name(options.db_name)
-        .read_timeout(options.read_timeout.map(Duration::from_secs_f32))
-        .write_timeout(options.write_timeout.map(Duration::from_secs_f32));
-
-    let pool = Pool::new_manual(
-        options.min_threads.unwrap_or(DEFAULT_MIN_THREADS),
-        options.max_threads.unwrap_or(DEFAULT_MAX_THREADS),
-        builder,
-    )?;
-
-    let handle = NEXT_ID.fetch_add(1, std::sync::atomic::Ordering::Relaxed);
-    POOL.insert(handle, pool);
-    Ok(json!({
-        "status": "ok",
-        "handle": handle.to_string(),
-    }))
-}
-
-fn do_query(handle: &str, query: &str, params: &str) -> Result<serde_json::Value, Box<dyn Error>> {
-    let mut conn = {
-        let pool = match POOL.get(&handle.parse()?) {
-            Some(s) => s,
-            None => return Ok(json!({"status": "offline"})),
-        };
-        pool.get_conn()?
-    };
-
+fn do_query(conn: &mut mysql::Conn, query: &str, params: &str) -> Result<serde_json::Value, Box<dyn Error>> {
     let query_result = conn.exec_iter(query, params_from_json(params))?;
     let affected = query_result.affected_rows();
     let last_insert_id = query_result.last_insert_id();
@@ -206,8 +303,6 @@ fn do_query(handle: &str, query: &str, params: &str) -> Result<serde_json::Value
         }
         rows.push(serde_json::Value::Array(json_row));
     }
-
-    drop(conn);
 
     Ok(json! {{
         "status": "ok",
